5096e0065aa4a85ff848f6cc0fd355f6f696c7e4
hasibur_rahman <hasiburuoda@gmail.com>
2023-06-09 12:06:05 +0600

    form validation done using form validation hook

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -12,6 +12,14 @@ const Login = () => {
         setShowPassword(!showPassword);
     }
 
+    const handleLoginForm = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+    }
+
 
     return (
         <div className='md:flex gap-20 w-9/12 mx-auto items-center justify-between'>
@@ -20,19 +28,19 @@ const Login = () => {
             </div>
             <div className='w-1/2 '>
                 <h2 className='text-4xl font-bold mb-4' >Login </h2>
-                <form>
+                <form onSubmit={handleLoginForm}>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Email</span>
                         </label>
-                        <input type="text" placeholder="email" className="input input-bordered" />
+                        <input name='email' type="text" placeholder="email" className="input input-bordered" required/>
                     </div>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Password</span>
                         </label>
                         <div className='flex'>
-                            <input type={showPassword ? 'text' : 'password'} placeholder="Pasword" className="input input-bordered w-full border-r-0 pass-input" />
+                            <input name='password' type={showPassword ? 'text' : 'password'} placeholder="Pasword" className="input input-bordered w-full border-r-0 pass-input" required/>
                             <button onClick={handleShowPasword} className='input input-bordered eye-icon'>
                                 {showPassword ? <FaEyeSlash /> : <FaEye />}
                             </button>
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -2,17 +2,28 @@ import React, { useState } from 'react';
 import loginImg from '../../assets/Images/login_page.png'
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
 import './Register.css'
-import { BsGoogle } from 'react-icons/bs';
 import { FcGoogle } from 'react-icons/fc';
+import { useForm } from "react-hook-form";
 
 const Register = () => {
 
     const [showPassword, setShowPassword] = useState(false);
-
     const handleShowPasword = event => {
         event.preventDefault();
         setShowPassword(!showPassword);
     }
+
+    // handle form submit with form submit hook:
+    const { register, reset, handleSubmit, formState: { errors } } = useForm();
+
+    const onSubmit = handleSubmit(data => {
+        console.log(data)
+
+        
+        // reset();
+    });
+
+
 
 
     return (
@@ -21,74 +32,93 @@ const Register = () => {
                 <img src={loginImg} alt="" />
             </div>
             <div className='md:w-1/2 '>
-                <h2 className='text-4xl font-bold mb-4' >Login </h2>
-                <form>
+                <h2 className='text-4xl font-bold mb-4' >Register User </h2>
+                <form onSubmit={handleSubmit(onSubmit)}>
                     <div className='md:grid grid-cols-2 gap-x-4'>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Name</span>
                             </label>
-                            <input type="text" placeholder="Name" className="input input-bordered" />
+                            <input {...register("name", { required: true })} type="text" placeholder="Name" className="input input-bordered" />
+                            {errors.name && <span className='text-rose-500'>Name is required</span>}
                         </div>
+
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="email" placeholder="Email" className="input input-bordered" />
+                            <input {...register("email", { required: true })} type="email" placeholder="Email" className="input input-bordered" />
+                            {errors.email && <span className='text-rose-500'>Email is required</span>}
+
                         </div>
+
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Phone</span>
                             </label>
-                            <input type="text" placeholder="Phone" className="input input-bordered" />
+                            <input {...register("phone")} type="text" placeholder="Phone" className="input input-bordered" />
                         </div>
+
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Address</span>
                             </label>
-                            <input type="text" placeholder="Address" className="input input-bordered" />
+                            <input {...register("address")} type="text" placeholder="Address" className="input input-bordered" />
                         </div>
+
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Photo URL</span>
                             </label>
-                            <input type="text" placeholder="Photo URL" className="input input-bordered" />
+                            <input {...register("photoUrl")} type="text" placeholder="Photo URL" className="input input-bordered" />
                         </div>
+
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Gender</span>
                             </label>
-                            <input type="text" placeholder="Gender" className="input input-bordered" />
+                            <input {...register("gender")} type="text" placeholder="Gender" className="input input-bordered" />
                         </div>
+
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
                             <div className='flex'>
-                                <input type={showPassword ? 'text' : 'password'} placeholder="Pasword" className="input input-bordered w-full border-r-0 pass-input" />
+                                <input {...register("password", {
+                                    required: true,
+                                    minLength: 6,
+                                    pattern: /(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/,
+
+                                })} type={showPassword ? 'text' : 'password'} placeholder="Pasword" className="input input-bordered w-full border-r-0 pass-input" />
                                 <button onClick={handleShowPasword} className='input input-bordered eye-icon'>
                                     {showPassword ? <FaEyeSlash /> : <FaEye />}
                                 </button>
                             </div>
+                            {errors.password === 'required' && <span className='text-rose-500 w-full '>Password is required</span>}
+                            {errors.password?.type === 'minLength' && <span className='text-rose-500 w-full '>Password must be 6 character</span>}
+                            {errors.password?.type === 'pattern' && <span className='text-rose-500 w-full '>Password must have one Capical & small letter and one number and spcial character</span>}
                         </div>
+
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Confirm Password</span>
                             </label>
                             <div className='flex'>
-                                <input type={showPassword ? 'text' : 'password'} placeholder="Confirm Password" className="input input-bordered w-full border-r-0 pass-input" />
+                                <input {...register("confirmPassword")} type={showPassword ? 'text' : 'password'} placeholder="Confirm Password" className="input input-bordered w-full border-r-0 pass-input" />
                                 <button onClick={handleShowPasword} className='input input-bordered eye-icon'>
                                     {showPassword ? <FaEyeSlash /> : <FaEye />}
                                 </button>
                             </div>
                         </div>
+
                     </div>
                     <button className='btn btn-warning w-full mt-8'>Register</button>
                     <div className="divider divider-vertical">OR</div>
-                    <div>
-                        <button className='btn  w-full '> <FcGoogle className='h-6 w-6' />Sign up using Google</button>
-                    </div>
                 </form>
+                <div>
+                    <button className='btn  w-full '> <FcGoogle className='h-6 w-6' />Sign up using Google</button>
+                </div>
             </div>
         </div>
     );
