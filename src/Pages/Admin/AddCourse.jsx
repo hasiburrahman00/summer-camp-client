1e04131621e79c8411e4cf30ee96302d8e128c1a
hasibur_rahman <hasiburuoda@gmail.com>
2023-06-12 21:11:11 +0600

    admin approval part done then class will be shown as a course

--- src/Pages/Admin/AddCourse.jsx
+++ src/Pages/Admin/AddCourse.jsx
@@ -1,27 +1,107 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../../Provider/AuthProvider';
+import axios from 'axios';
+import Swal from 'sweetalert2';
 
 const AddCourse = () => {
     const { user } = useContext(AuthContext);
     console.log(user)
 
+    const handleAddCourse = event => {
+        event.preventDefault();
+        const form = event.target;
+        console.log(form);
+        const instructorName = form.instructorName.value;
+        const instructorEmail = form.instructorEmail.value;
+        const courseName = form.courseName.value;
+        const price = form.price.value;
+        const availableSeats = form.seat.value;
+        const courseImage = form.photoURL.value;
+        const courseInfo = { instructorName, instructorEmail, courseName, price, availableSeats, courseImage, status: 'pending', photoURL: user.photoURL, enrolled: 0 }
+        console.log(courseInfo)
+
+        fetch('http://localhost:5000/courses', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(courseInfo),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.insertedId) {
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Course Added Successfully. waiting to Admins under review and publish finally',
+                        showConfirmButton: true,
+                    })
+                    form.reset();
+                }
+            })
+            .catch((error) => {
+                // Handle any errors that occurred during the request
+                console.error('Error posting data:', error);
+            });
+
+
+    }
+
 
     return (
         <div className='w-10/12 mx-auto p-20 rounded-lg bg-slate-300'>
             <h2 className='text-3xl font-bold uppercase text-center mb-12'>Add Course </h2>
-            <form >
-                <div className='grid grid-cols-2 gap-4'>
-                    <input defaultValue={user?.displayName} type="text" placeholder="Type here" className="input input-bordered input-success w-full " readOnly />
-                    <input defaultValue={user?.email} type="text" placeholder="Type here" className="input input-bordered input-success w-full " readOnly />
-                    <input type="text" placeholder="Course Name" className="input input-bordered input-success w-full " />
-                    <input type="text" placeholder="price " className="input input-bordered input-success w-full " />
-                    <input type="text" placeholder="Available Seat" className="input input-bordered input-success w-full" />
-                    <input type="text" placeholder="Course Image URL" className="input input-bordered input-success w-full" />
+            <form onSubmit={handleAddCourse}>
+                <div className='grid grid-cols-2 gap-2 gap-x-4'>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Name</span>
+                        </label>
+                        <input name='instructorName' defaultValue={user?.displayName} type="text" placeholder="Type here" className="input input-bordered input-success w-full " readOnly />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input name='instructorEmail' defaultValue={user?.email} type="text" placeholder="Type here" className="input input-bordered input-success w-full " readOnly />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Course Name</span>
+                        </label>
+                        <input name='courseName' type="text" placeholder="Course Name" className="input input-bordered input-success w-full " required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Price</span>
+                        </label>
+                        <input name='price' type="text" placeholder="price " className="input input-bordered input-success w-full " required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Available Seat</span>
+                        </label>
+                        <input name='seat' type="text" placeholder="Available Seat" className="input input-bordered input-success w-full" required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Course Image</span>
+                        </label>
+                        <input name='photoURL' type="text" placeholder="Course Image URL" className="input input-bordered input-success w-full" required />
+                    </div>
                 </div>
-                <input className='btn btn-success w-full mt-8 font-bold text-black' type="button" value="Add Course" />
+                <button className='btn btn-success w-full mt-8 font-bold text-black'>Add Course </button>
             </form>
         </div>
+
     );
 };
 
 export default AddCourse;
+
+
+
+
+
+
+
+
--- src/Pages/Admin/ManageCourse.jsx
+++ src/Pages/Admin/ManageCourse.jsx
@@ -1,9 +1,127 @@
-import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import React, { useState } from 'react';
+import { json } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 const ManageCourse = () => {
+
+    const { data: courses = [], refetch } = useQuery(['courses'], async () => {
+        const res = await fetch('http://localhost:5000/manageCourse')
+        return res.json();
+    })
+
+    const handleDeny = async (id) => {
+        console.log(id);
+        const { value: text } = await Swal.fire({
+            input: 'textarea',
+            inputLabel: 'Message',
+            inputPlaceholder: 'Type your message here...',
+            inputAttributes: {
+                'aria-label': 'Type your message here'
+            },
+            showCancelButton: true
+        })
+        let feedbacks = { feedback: '' };
+        if (text) {
+            feedbacks = { feedback: text }
+        }
+
+        fetch(`http://localhost:5000/courses/${id}`, {
+            method: 'PATCH',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(feedbacks)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.modifiedCount > 0) {
+                    refetch();
+                    Swal.fire({
+                        position: 'top-end',
+                        icon: 'success',
+                        title: 'Course review has been successfull ',
+                        showConfirmButton: false,
+                        timer: 1500
+                    })
+                }
+            })
+
+
+
+    }
+
+
+
     return (
-        <div>
-            
+        <div className='w-11/12 mx-auto bg-orange-100 p-12 rounded-lg'>
+
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>
+                                <label>
+                                    #SI
+                                </label>
+                            </th>
+                            <th>Name</th>
+                            <th>Instructor Info </th>
+                            <th>Available Seat</th>
+                            <th>Approval Status</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        {
+                            courses.map((course, index) => <tr key={course._id}>
+                                <th>
+                                    <label>
+                                        {index + 1}
+                                    </label>
+                                </th>
+                                <td>
+                                    <div className="flex items-center space-x-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle w-12 h-12">
+                                                <img className='w-full h-full' src={course?.courseImage} alt="Avatar Tailwind CSS Component" />
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <div className="font-bold text-xl">{course?.courseName}</div>
+                                            <div className="badge badge-ghost badge-sm font-semibold">price : ${course?.price} </div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td className='flex items-center  space-x-3'>
+                                    <div>
+                                        <img className='h-8 w-8 rounded-xl' src={course?.photoURL} alt="" />
+                                    </div>
+                                    <div>
+                                        <span className='font-bold'>{course?.instructorName}</span>
+                                        <br />
+                                        <span className="badge badge-ghost badge-sm">{course?.instructorEmail}</span>
+                                    </div>
+                                </td>
+                                <td>
+                                    <span className='text-lg text-green-500 font-semibold'>{course?.availableSeats}</span>
+                                </td>
+                                <td>
+                                    <h2>{course?.status}</h2>
+                                </td>
+                                <th className='flex flex-col space-y-2'>
+                                    <button disabled={`${course.status === 'Deny' ? 'disabled' : ''}`} onClick={() => handleDeny(course?._id)} className="btn btn-error btn-xs">Deny</button>
+                                    <button disabled={`${course.status === 'Deny' ? 'disabled' : ''}`} className="btn btn-success btn-xs">Approve</button>
+                                </th>
+                            </tr>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
+
         </div>
     );
 };
